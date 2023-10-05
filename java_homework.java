a08924e7c422398b1b066398399acaab87d0c86c
Jimmy0108 <75895765+Jimmy0108@users.noreply.github.com>
2023-10-05 15:20:06 +0800

    Update java_homework.java

--- java_homework.java
+++ java_homework.java
@@ -1,1 +1,34 @@
-
+import java.lang.*;
+import java.util.Arrays;
+import java.io.Console;
+public class bublesort
+{
+    public static void main(String args[])
+    {
+        int sum = 0;
+        Console console = System.console();
+        int numarray[] = new int[10];
+        for(int i=0;i<6;i++)
+        {
+            numarray[i] = Integer.parseInt(console.readLine());
+        }
+        /*for(int i=0;i<numarray.length;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                if(numarray[j] < numarray[j+1])
+                {
+                    int num = 0;
+                    num = numarray[j];
+                    numarray[j] = numarray[j+1];
+                    numarray[j+1] = num;
+                }
+            }
+        }*/
+        Arrays.sort(numarray);
+        for(int i=0;i<numarray.length;i++)
+        {
+            System.out.println(numarray[i] + " ");
+        }
+    }
+}
